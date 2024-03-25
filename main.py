06c6d9d388e53048c098f54316883174b58a13ec
Brenton <brtnlaw@gmail.com>
2024-03-24 21:36:11 -0500

    clean up main

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-from top_of_book import get_game_info, get_markets, get_most_recent_odds, get_nba_games, get_nfl_games
-from scrape_data import scrape_NBA_team_data
-
 def main():
     print("Hello World!")
 
