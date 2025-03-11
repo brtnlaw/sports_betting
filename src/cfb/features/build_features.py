88c36c683b802726e238d30ec0fc5bdf9c8c5def
Brenton <brtnlaw@gmail.com>
2025-03-10 19:26:36 -0700

    finish preprocessing, add the basics of model. need to work on cv next time

--- src/cfb/features/build_features.py
+++ src/cfb/features/build_features.py
@@ -26,26 +26,167 @@ class Preprocessing(Feature):
         Feature: The base feature engineering class.
     """
 
-    def remove_nan_rows(df):
-        # Gets rid of empty home and away points and quarterly data
-        df = df.dropna(subset=["home_points", "away_points"])
-        df = df[
-            df["home_line_scores"].apply(lambda x: x != [])
-            & df["away_line_scores"].apply(lambda x: x != [])
+    def clean_cols(self):
+        # Gets rid of columns with no predictive value and fixes typing
+        useless_cols = [
+            "id",
+            "start_time_tbd",
+            "completed",
+            "home_id",
+            "home_pregame_win_probability",
+            "home_postgame_win_probability",
+            "home_postgame_elo",
+            "away_id",
+            "away_pregame_win_probability",
+            "away_postgame_win_probability",
+            "away_postgame_elo",
+            "venue_id",
+            "venue",
+            "highlights",
+            "notes",
+            "id_venue",
+            "name",
+            "city",
+            "state",
+            "zip",
+            "countrycode",
         ]
-        return df
+        self.df = self.df.drop(
+            columns=[col for col in useless_cols if col in self.df.columns]
+        )
+        self.df["neutral_site"] = self.df["neutral_site"].apply(int)
+        self.df["conference_game"] = self.df["conference_game"].apply(int)
+        self.df.fillna(0)
 
-    def encode_categorical_cols(df):
-        # TODO: ensure that the names are separated by _
-        pass
+    def remove_nan_rows(self):
+        # Gets rid of empty home and away points and quarterly data
+        self.df = self.df.dropna(
+            subset=[
+                "home_points",
+                "away_points",
+                "home_line_scores",
+                "away_line_scores",
+                "home_days_since_last_game",
+                "away_days_since_last_game",
+            ]
+        )
+        self.df = self.df[
+            self.df["home_line_scores"].apply(lambda x: len(x) >= 4)
+            & self.df["away_line_scores"].apply(lambda x: len(x) >= 4)
+        ]
+        # TODO: for each nan attendance, for every team, average the attendance for filling in NaN's all time
+        # TODO: for each nan elo & excitement index, for every team, get a rolling average of the previous.
+        # for now, just to get things going, we fill with 0
+        self.df.fillna(0, inplace=True)
+
+    def encode_categorical_cols(self):
+        # One hot encodes, then drops all categorical columns
+        exclude_cols = [
+            "home_line_scores",
+            "away_line_scores",
+            "start_date",
+            "highlights",
+            "is_grass",
+        ]
+        categorical_cols = [
+            col
+            for col in self.df.select_dtypes(include=["object", "category"]).columns
+            if col not in exclude_cols
+        ]
+        self.df = pd.get_dummies(self.df, columns=categorical_cols, dtype=int)
+        self.df.columns = self.df.columns.str.lower().str.replace(" ", "_")
+
+    def expand_quarters(self):
+        # Splits out the list of scores into four columns
+        self.df["ot"] = self.df["home_line_scores"].apply(lambda x: int(len(x) > 4))
+        for prefix in ["home", "away"]:
+            line_score = f"{prefix}_line_scores"
+            self.df[line_score] = self.df[line_score].apply(
+                lambda x: x[0:4] + [sum(x[5:])] if len(x) >= 5 else x + [0]
+            )
+            self.df[
+                [
+                    f"{prefix}_q1",
+                    f"{prefix}_q2",
+                    f"{prefix}_q3",
+                    f"{prefix}_q4",
+                    f"{prefix}_ot",
+                ]
+            ] = pd.DataFrame(self.df[line_score].tolist(), index=self.df.index)
+            self.df[f"{prefix}_h1"] = self.df[f"{prefix}_q1"] + self.df[f"{prefix}_q2"]
+            self.df[f"{prefix}_h2"] = self.df[f"{prefix}_q3"] + self.df[f"{prefix}_q4"]
+            self.df.drop(
+                [
+                    line_score,
+                    f"{prefix}_points",
+                    f"{prefix}_q1",
+                    f"{prefix}_q2",
+                    f"{prefix}_q3",
+                    f"{prefix}_q4",
+                ],
+                axis=1,
+                inplace=True,
+            )
+
+    def date_to_days_since(self):
+        # Create df_home and df_away as before
+        df_home = self.df[["start_date", "home_team"]].rename(
+            columns={"home_team": "team"}
+        )
+        df_away = self.df[["start_date", "away_team"]].rename(
+            columns={"away_team": "team"}
+        )
+
+        # Concatenate home and away data to create a single list of all games for each team
+        df_combined = pd.concat([df_home, df_away])
+
+        # Sort by team and start_date
+        df_combined = df_combined.sort_values(by=["team", "start_date"])
+
+        # Calculate the previous game for each team
+        df_combined["previous_game"] = df_combined.groupby("team")["start_date"].shift(
+            1
+        )
+
+        # Merge the previous game data for home team
+        self.df["previous_game"] = None
+        for side in ["home", "away"]:
+            self.df = self.df.merge(
+                df_combined[["team", "start_date", "previous_game"]],
+                left_on=[f"{side}_team", "start_date"],
+                right_on=["team", "start_date"],
+                how="left",
+                suffixes=("", f"_{side}"),
+            )
+            self.df[f"{side}_days_since_last_game"] = (
+                self.df["start_date"] - self.df[f"previous_game_{side}"]
+            ).apply(lambda x: x.days if pd.notna(x) else None)
+
+        # Drop temporary columns from merge
+        self.df.drop(
+            columns=[
+                "team",
+                "previous_game",
+                "previous_game_home",
+                "team_away",
+                "previous_game_away",
+                "start_date",
+            ],
+            inplace=True,
+        )
 
     def transform(self) -> pd.DataFrame:
-        pass
+        self.clean_cols()
+        self.date_to_days_since()  # Goes before encoding
+        self.remove_nan_rows()  # Goes after date to days since, removes first instance of a team (no data)
+        self.encode_categorical_cols()
+        self.expand_quarters()
+        return self.df
 
 
 class OffensiveFeatures(Feature):
     # TODO: Kalman filter for scoring in each quarter
-    # TODO: Percent of yards come from rushing/passing
+    # TODO: Percent of yards come from rushing/passing - both Offense and Defense
     pass
 
 
--- src/cfb/model/train.py
+++ src/cfb/model/train.py
@@ -5,17 +5,31 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score, mean_squared_error, mean_absolute_error
 import lightgbm as lgb
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, TimeSeriesSplit
 
-MODELS = {
-    "linear_regression": LinearRegression(),
-    "light_gbm": lgb
-}
+MODELS = {"linear_regression": LinearRegression(), "light_gbm": lgb}
 RANDOM_SEED = 12345
+
+
+def cv(X, y):
+    tscv = TimeSeriesSplit(5)
+    for fold, (train_index, test_index) in enumerate(tscv.split(X)):
+        print("Fold: {}".format(fold))
+        print("TRAIN indices:", train_index, "\n", "TEST indices:", test_index)
+        print("\n")
+        X_train, X_test = X.iloc[train_index], X.iloc[test_index]
+        y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+        # TODO: open question - should my target be making the best bets? or just prediction straight up?
+        # If the former, then need a step here regarding my new y_train -->
+        # Something like 2013-2017 train my model, then
+    pass
+
 
 def train_model(X_train: pd.DataFrame, y_train: pd.Series, model_name="light_gbm"):
     if model_name not in MODELS:
-        raise ValueError(f"Model '{model_name}' not recognized. Choose from {list(MODELS.keys())}")
+        raise ValueError(
+            f"Model '{model_name}' not recognized. Choose from {list(MODELS.keys())}"
+        )
     model = MODELS[model_name]
     if model_name in ["linear_regression"]:
         model.fit(X_train, y_train)
@@ -32,24 +46,42 @@ def train_model(X_train: pd.DataFrame, y_train: pd.Series, model_name="light_gbm
             "verbose": 0,
         }
         lgb_train = lgb.Dataset(X_train, y_train)
-        model = model.train( 
-            params, lgb_train
-        )
+        model = model.train(params, lgb_train)
     return model
 
+
+# TODO: set up a baseline and compare, so do something like time based of ---train-----test-future
 def evaluate_model(model, X_test, y_test):
     y_pred = model.predict(X_test)
     r2 = r2_score(y_test, y_pred)
     mse = mean_squared_error(y_test, y_pred)
     mae = mean_absolute_error(y_test, y_pred)
+    if model.__class__.__name__ == "Booster":
+        # Get the most predictive features
+        feature_importances = model.feature_importance(importance_type="gain")
+        feature_names = model.feature_name()
+        importance_df = pd.DataFrame(
+            {"Feature": feature_names, "Importance": feature_importances}
+        )
+        importance_df["Percentage"] = (
+            importance_df["Importance"] / importance_df["Importance"].sum()
+        ) * 100
+        top_n_features = importance_df.sort_values(
+            by="Importance", ascending=False
+        ).head(5)
+        print(top_n_features[["Feature", "Percentage"]])
+    print("===============================")
     print(f"Model Performance ({model.__class__.__name__}):")
     print("R2 Score:\n", r2)
     print("MSE:\n", mse)
     print("MAE:\n", mae)
 
-# TODO: add in all the features
-def train_and_pkl(X, y, model_name, pkl_name="test"):
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=RANDOM_SEED)
+
+def train_and_pkl(X, y, model_name="light_gbm", pkl_name="test"):
+    # TODO: TimeSeriesSplit this, but in each fold, what bet would be placed?
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=RANDOM_SEED
+    )
     model = train_model(X_train, y_train, model_name)
     evaluate_model(model, X_test, y_test)
     with open(f"src/cfb/model/models/{pkl_name}.pkl", "wb") as f:
